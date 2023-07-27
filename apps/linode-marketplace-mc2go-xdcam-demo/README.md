cc8aac831f1404462761e5443dc6b7198d6ca50b
Tsviatko Jongov <jongov@gmail.com>
2023-07-27 08:57:33 +0300

    MainConcept 2GO XDCAM Transcoder Demo integration
    MainConcept 2GO XDCAM Transcoder Demo integration

---
+++ apps/linode-marketplace-mc2go-xdcam-demo/README.md
@@ -0,0 +1,35 @@
+## App Information
+MainConcept 2GO XDCAM Transcoder
+
+### App Name
+MainConcept 2GO XDCAM Transcoder
+
+### App description
+[MainConcept 2GO XDCAM Transcoder](https://www.mainconcept.com/mc2go) is an optimized Docker container for file-based transcoding of media files into professional Sony camera formats like XDCAM HD, XDCAM EX, XDCAM IMX and DVCAM (XDCAM DV).
+
+### Version Number
+2.3
+
+### Support
+* [Documentation](https://www.mainconcept.com/mc2go)
+
+### Operating Systems
+Ubuntu 20.04 LTS, Ubuntu 22.04 LTS, Debian 11
+
+### Documentation
+[Documentation](https://www.mainconcept.com/mc2go)
+
+### Brand color 1
+Color code - #041125
+
+### Brand color 2
+Color code - #6DBA98
+
+### Logo
+Light and dark logo included in assets folder.
+
+Light:
+![light](assets/MC2GOXDCAM_White.svg)
+
+Dark:
+![dark](assets/MC2GOXDCAM.svg)
---
+++ deployment_scripts/linode-marketplace-mc2go-xdcam-demo/mc2go-xdcam-demo-deploy.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+trap "cleanup $? $LINENO" EXIT
+
+## MainConcept MC2GO XDCAM Transcoder Demo Settings
+#<UDF name="mc2go_port" Label="MC2GO XDCAM Transcoder Port" example="Default: 8080" default="8080" /> 
+
+# git repo
+#export GIT_REPO="https://github.com/linode-solutions/marketplace-apps.git"
+
+#test git repo
+export GIT_REPO="https://github.com/jongov/marketplace-apps.git"
+export BRANCH="develop"
+
+export WORK_DIR="/tmp/marketplace-apps"
+export MARKETPLACE_APP="apps/linode-marketplace-mc2go-xdcam-demo"
+
+# enable logging
+exec > >(tee /dev/ttyS0 /var/log/stackscript.log) 2>&1
+
+function cleanup {
+  if [ -d "${WORK_DIR}" ]; then
+    rm -rf ${WORK_DIR}
+  fi
+
+}
+
+function udf {
+  local group_vars="${WORK_DIR}/${MARKETPLACE_APP}/group_vars/linode/vars"
+  sed 's/  //g' <<EOF > ${group_vars}
+
+  # deployment vars
+  mc2go_port: ${MC2GO_PORT}
+EOF
+
+}
+
+function run {
+  # install dependancies
+  apt-get update
+  apt-get install -y git python3 python3-pip
+
+  # clone repo and set up ansible environment
+  # git -C /tmp clone ${GIT_REPO}
+  # for a single testing branch
+  git -C /tmp clone --single-branch --branch ${BRANCH} ${GIT_REPO}
+
+  # venv
+  cd ${WORK_DIR}/${MARKETPLACE_APP}
+  pip3 install virtualenv
+  python3 -m virtualenv env
+  source env/bin/activate
+  pip install pip --upgrade
+  pip install -r requirements.txt
+  ansible-galaxy install -r collections.yml
+
+  # populate group_vars
+  udf
+  # run playbooks
+  for playbook in provision.yml site.yml; do ansible-playbook -vvvv $playbook; done
+}
+
+function installation_complete {
+  cat << EOF
+#########################
+# INSTALLATION COMPLETE #
+############################################
+# * Hugs are worth more than handshakes *  #
+############################################
+EOF
+}
+# main
+run && installation_complete
+cleanup
