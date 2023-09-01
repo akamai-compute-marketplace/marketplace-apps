cbceae8770b9e6f2450f3f870239ce3e9f01df16
Juan Coto <jcotsan@gmail.com>
2023-09-01 14:48:10 -0600

    marketplace app fix on deploy

--- deployment_scripts/linode-marketplace-uptimekuma/uptimekuma-deploy.sh
+++ deployment_scripts/linode-marketplace-uptimekuma/uptimekuma-deploy.sh
@@ -20,7 +20,7 @@ trap "cleanup $? $LINENO" EXIT
 # git repo
 export GIT_REPO="https://github.com/jcotoBan/marketplace-apps.git"
 export WORK_DIR="/tmp/marketplace-apps" 
-export MARKETPLACE_APP="apps/linode-marketplace-oodo"
+export MARKETPLACE_APP="apps/linode-marketplace-uptimekuma"
 
 # enable logging
 exec > >(tee /dev/ttyS0 /var/log/stackscript.log) 2>&1
