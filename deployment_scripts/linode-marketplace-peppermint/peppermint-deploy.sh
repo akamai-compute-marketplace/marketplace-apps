54acc1ee5c353cc9a405df36f868e0ccb084d88e
Holden Morris <morrisholdenx@gmail.com>
2023-09-19 13:55:47 -0400

    add nginx reverse proxy & match style

--- deployment_scripts/linode-marketplace-peppermint/peppermint-deploy.sh
+++ deployment_scripts/linode-marketplace-peppermint/peppermint-deploy.sh
@@ -2,14 +2,22 @@
 set -e
 trap "cleanup $? $LINENO" EXIT
 
-##Linode/SSH security settings
+## Linode/SSH security settings
 #<UDF name="user_name" label="The limited sudo user to be created for the Linode" default="">
 #<UDF name="password" label="The password for the limited sudo user" example="an0th3r_s3cure_p4ssw0rd" default="">
 #<UDF name="disable_root" label="Disable root access over SSH?" oneOf="Yes,No" default="No">
 #<UDF name="pubkey" label="The SSH Public Key that will be used to access the Linode (Recommended)" default="">
 
+## Domain Settings
+#<UDF name="token_password" label="Your Linode API token. This is needed to create your WordPress server's DNS records" default="">
+#<UDF name="subdomain" label="Subdomain" example="The subdomain for the DNS record: www (Requires Domain)" default="">
+#<UDF name="domain" label="Domain" example="The domain for the DNS record: example.com (Requires API token)" default="">
+
+## Peppermint setup
+#<UDF name="soa_email_address" label="Email address (for the Let's Encrypt SSL certificate)" example="user@domain.tld">
+
 # git repo
-export GIT_REPO="https://github.com/jcotoBan/marketplace-apps.git"
+export GIT_REPO="https://github.com/akamai-compute-marketplace/marketplace-apps.git"
 export WORK_DIR="/tmp/marketplace-apps" 
 export MARKETPLACE_APP="apps/linode-marketplace-peppermint"
 
@@ -26,14 +34,16 @@ function cleanup {
 function udf {
   
   local group_vars="${WORK_DIR}/${MARKETPLACE_APP}/group_vars/linode/vars"
+  echo "webserver_stack: lemp" >> ${group_vars};
 
   if [[ -n ${USER_NAME} ]]; then
     echo "username: ${USER_NAME}" >> ${group_vars};
   else echo "No username entered";
   fi
 
-    if [[ -n ${DISABLE_ROOT} ]]; then
-    echo "disable_root: ${DISABLE_ROOT}" >> ${group_vars};
+  if [ "$DISABLE_ROOT" = "Yes" ]; then
+    echo "disable_root: yes" >> ${group_vars};
+  else echo "Leaving root login enabled";
   fi
 
   if [[ -n ${PASSWORD} ]]; then
@@ -45,6 +55,26 @@ function udf {
     echo "pubkey: ${PUBKEY}" >> ${group_vars};
   else echo "No pubkey entered";
   fi
+
+  if [[ -n ${SOA_EMAIL_ADDRESS} ]]; then
+    echo "soa_email_address: ${SOA_EMAIL_ADDRESS}" >> ${group_vars};
+  fi
+
+  if [[ -n ${DOMAIN} ]]; then
+    echo "domain: ${DOMAIN}" >> ${group_vars};
+  else
+    echo "default_dns: $(hostname -I | awk '{print $1}'| tr '.' '-' | awk {'print $1 ".ip.linodeusercontent.com"'})" >> ${group_vars};
+  fi
+
+  if [[ -n ${SUBDOMAIN} ]]; then
+    echo "subdomain: ${SUBDOMAIN}" >> ${group_vars};
+  else echo "subdomain: www" >> ${group_vars};
+  fi
+
+  if [[ -n ${TOKEN_PASSWORD} ]]; then
+    echo "token_password: ${TOKEN_PASSWORD}" >> ${group_vars};
+  else echo "No API token entered";
+  fi
 }
 
 function run {
@@ -53,10 +83,9 @@ function run {
   apt-get install -y git python3 python3-pip
 
   # clone repo and set up ansible environment
-  git -C /tmp clone --depth 1 --filter=blob:none ${GIT_REPO} --branch peppermint --sparse
-  cd ${WORK_DIR}
-  git sparse-checkout init --cone
-  git sparse-checkout set apps/linode-marketplace-peppermint apps/linode_helpers
+  git -C /tmp clone ${GIT_REPO}
+  # for a single testing branch
+  # git -C /tmp clone --single-branch --branch ${BRANCH} ${GIT_REPO}
 
   # venv
   cd ${WORK_DIR}/${MARKETPLACE_APP}
