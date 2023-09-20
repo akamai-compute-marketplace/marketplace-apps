70bcf2796e2b0af3eeb0de73e5cd10e2d900f8c8
Holden Morris <morrisholdenx@gmail.com>
2023-09-20 14:00:18 -0400

    nginx & docker cleanup

--- deployment_scripts/linode-marketplace-yacht/yacht-deploy.sh
+++ deployment_scripts/linode-marketplace-yacht/yacht-deploy.sh
@@ -8,6 +8,12 @@ trap "cleanup $? $LINENO" EXIT
 #<UDF name="disable_root" label="Disable root access over SSH?" oneOf="Yes,No" default="No">
 #<UDF name="pubkey" label="The SSH Public Key that will be used to access the Linode (Recommended)" default="">
 
+## Domain Settings
+#<UDF name="token_password" label="Your Linode API token. This is needed to create your server's DNS records" default="">
+#<UDF name="subdomain" label="Subdomain" example="The subdomain for the DNS record: www (Requires Domain)" default="">
+#<UDF name="domain" label="Domain" example="The domain for the DNS record: example.com (Requires API token)" default="">
+#<UDF name="soa_email_address" label="Email address (for the Let's Encrypt SSL certificate)" example="user@domain.tld">
+
 ## Yacht Settings 
 #<UDF name="yemail" Label="Yacht Email" example="admin@yacht.local" default="admin@yacht.local" />
 #<UDF name="ypassword" Label="Yacht Password" example="Password" />
@@ -37,8 +43,9 @@ function udf {
   else echo "No username entered";
   fi
 
-    if [[ -n ${DISABLE_ROOT} ]]; then
-    echo "disable_root: ${DISABLE_ROOT}" >> ${group_vars};
+  if [ "$DISABLE_ROOT" = "Yes" ]; then
+    echo "disable_root: yes" >> ${group_vars};
+  else echo "Leaving root login enabled";
   fi
 
   if [[ -n ${PASSWORD} ]]; then
@@ -68,6 +75,26 @@ function udf {
   if [[ -n ${YTHEME} ]]; then
     echo "yacht_theme: ${YTHEME}" >> ${group_vars};
   fi
+
+  if [[ -n ${SOA_EMAIL_ADDRESS} ]]; then
+    echo "soa_email_address: ${SOA_EMAIL_ADDRESS}" >> ${group_vars};
+  fi
+
+  if [[ -n ${DOMAIN} ]]; then
+    echo "domain: ${DOMAIN}" >> ${group_vars};
+  else
+    echo "default_dns: $(hostname -I | awk '{print $1}'| tr '.' '-' | awk {'print $1 ".ip.linodeusercontent.com"'})" >> ${group_vars};
+  fi
+
+  if [[ -n ${SUBDOMAIN} ]]; then
+    echo "subdomain: ${SUBDOMAIN}" >> ${group_vars};
+  else echo "subdomain: www" >> ${group_vars};
+  fi
+
+  if [[ -n ${TOKEN_PASSWORD} ]]; then
+    echo "token_password: ${TOKEN_PASSWORD}" >> ${group_vars};
+  else echo "No API token entered";
+  fi
 }
 
 function run {
@@ -76,10 +103,9 @@ function run {
   apt-get install -y git python3 python3-pip
 
   # clone repo and set up ansible environment
-  git -C /tmp clone --depth 1 --filter=blob:none ${GIT_REPO} --branch yacht --sparse
-  cd ${WORK_DIR}
-  git sparse-checkout init --cone
-  git sparse-checkout set apps/linode-marketplace-yacht apps/linode_helpers
+  git -C /tmp clone ${GIT_REPO}
+  # for a single testing branch
+  # git -C /tmp clone --single-branch --branch ${BRANCH} ${GIT_REPO}
 
   # venv
   cd ${WORK_DIR}/${MARKETPLACE_APP}
