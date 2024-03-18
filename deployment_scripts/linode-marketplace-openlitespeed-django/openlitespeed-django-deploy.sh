007cf289620f04a3005832a326c79faaaf495368
Joseph Cardillo <joseph.s.cardillo@gmail.com>
2024-03-18 13:26:19 -0400

    initial commit for openlitespeed django

---
+++ deployment_scripts/linode-marketplace-openlitespeed-django/openlitespeed-django-deploy.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+trap "cleanup $? $LINENO" EXIT
+
+# git repo
+export GIT_REPO="https://github.com/akamai-compute-marketplace/marketplace-apps.git"
+export WORK_DIR="/tmp/marketplace-apps" 
+export MARKETPLACE_APP="apps/linode-marketplace-openlitespeed"
+
+# enable logging
+exec > >(tee /dev/ttyS0 /var/log/stackscript.log) 2>&1
+
+function cleanup {
+  if [ -d "${WORK_DIR}" ]; then
+    rm -rf ${WORK_DIR}
+  fi
+}
+
+function udf {
+  
+  local group_vars="${WORK_DIR}/${MARKETPLACE_APP}/group_vars/linode/vars"
+  sed 's/  //g' <<EOF > ${group_vars}
+EOF
+
+}
+
+function run {
+  # install dependancies
+  apt-get update
+  apt-get install -y git python3 python3-pip
+
+  # clone repo and set up ansible environment
+  git -C /tmp clone ${GIT_REPO}
+  # for a single testing branch
+  # git -C /tmp clone -b ${BRANCH} ${GIT_REPO}
+
+  # venv
+  cd ${WORK_DIR}/${MARKETPLACE_APP}
+  pip3 install virtualenv
+  python3 -m virtualenv env
+  source env/bin/activate
+  pip install pip --upgrade
+  pip install -r requirements.txt
+  ansible-galaxy install -r collections.yml
+
+  # populate group_vars
+  udf
+  # run playbooks
+  for playbook in provision.yml site.yml; do ansible-playbook -v $playbook; done
+  
+}
+
+function installation_complete {
+  echo "Installation Complete"
+}
+# main
+run && installation_complete
+cleanup
