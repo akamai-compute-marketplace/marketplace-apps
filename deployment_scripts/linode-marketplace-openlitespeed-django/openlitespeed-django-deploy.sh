1d7f0e9ab1f66b95203ee77bf3301643c15ad73b
Joseph Cardillo <joseph.s.cardillo@gmail.com>
2024-04-18 11:51:14 -0400

    Add SOA email addr for lets encrypt

--- deployment_scripts/linode-marketplace-openlitespeed-django/openlitespeed-django-deploy.sh
+++ deployment_scripts/linode-marketplace-openlitespeed-django/openlitespeed-django-deploy.sh
@@ -12,6 +12,7 @@ trap "cleanup $? $LINENO" EXIT
 #<UDF name="token_password" label="Your Linode API token. This is needed to create your Linode's DNS records" default="">
 #<UDF name="subdomain" label="Subdomain" example="The subdomain for the DNS record. `www` will be entered if no subdomain is supplied (Requires Domain)" default="">
 #<UDF name="domain" label="Domain" example="The domain for the DNS record: example.com (Requires API token)" default="">
+#<UDF name="soa_email_address" label="Email address (for the Let's Encrypt SSL certificate)" example="user@domain.tld" />
 
 # git repo
 export GIT_REPO="https://github.com/akamai-compute-marketplace/marketplace-apps.git"
