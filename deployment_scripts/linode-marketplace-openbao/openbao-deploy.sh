486a9e9e0f7a7a80a1d01aa2c3413e455a8cc0b0
hmorris3293 <morrisholdenx@gmail.com>
2024-07-02 09:29:14 -0400

    remove sudo
    Co-authored-by: n0vabyte <94801247+n0vabyte@users.noreply.github.com>

--- deployment_scripts/linode-marketplace-openbao/openbao-deploy.sh
+++ deployment_scripts/linode-marketplace-openbao/openbao-deploy.sh
@@ -116,7 +116,7 @@ function configure_privateip {
           echo "[warn] No private IP found. Adding.."
           add_privateip
           LINODE_IP=$(get_privateip)
-          echo "Address=$LINODE_IP/17" | sudo tee -a /etc/systemd/network/05-eth0.network
+          echo "Address=$LINODE_IP/17" | tee -a /etc/systemd/network/05-eth0.network
           systemctl restart systemd-networkd    
   fi
 }
