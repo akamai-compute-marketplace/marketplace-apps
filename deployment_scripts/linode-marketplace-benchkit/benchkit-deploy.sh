2e0bc09e1864443e72707b12ccab63762fe39c52
Elvis Segura <esegura@akamai.com>
2024-11-07 11:43:48 -0500

    update versions

--- deployment_scripts/linode-marketplace-benchkit/benchkit-deploy.sh
+++ deployment_scripts/linode-marketplace-benchkit/benchkit-deploy.sh
@@ -74,8 +74,8 @@ function run {
 
   # venv
   cd ${WORK_DIR}/${MARKETPLACE_APP}
-  pip3 install virtualenv
-  python3 -m virtualenv env
+  pip3 install python3-venv
+  python3 -m venv env
   source env/bin/activate
   pip install pip --upgrade
   pip install -r requirements.txt
