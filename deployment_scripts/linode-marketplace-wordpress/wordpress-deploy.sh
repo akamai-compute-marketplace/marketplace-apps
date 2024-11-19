0c092b27225d7f8fe7b877b0073dab0042750e5e
hmorris <hmorris@akamai.com>
2024-11-19 10:28:33 -0500

    changes for ubuntu24

--- deployment_scripts/linode-marketplace-wordpress/wordpress-deploy.sh
+++ deployment_scripts/linode-marketplace-wordpress/wordpress-deploy.sh
@@ -89,8 +89,8 @@ function run {
 
   # venv
   cd ${WORK_DIR}/${MARKETPLACE_APP}
-  pip3 install virtualenv
-  python3 -m virtualenv env
+  apt install python3-venv -y
+  python3 -m venv env
   source env/bin/activate
   pip install pip --upgrade
   pip install -r requirements.txt
