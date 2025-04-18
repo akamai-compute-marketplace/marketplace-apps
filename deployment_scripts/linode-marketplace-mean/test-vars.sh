83ba5e9420872755307cc1cb6605ec0c49cec7a8
Elvis Segura <esegura@akamai.com>
2025-04-18 13:54:06 -0400

    all updated

--- deployment_scripts/linode-marketplace-mean/test-vars.sh
+++ deployment_scripts/linode-marketplace-mean/test-vars.sh
@@ -4,7 +4,7 @@ DEFAULT_DNS="$(hostname -I | awk '{print $1}'| tr '.' '-' | awk {'print $1 ".ip.
 
 declare -A UDF_VARS
 UDF_VARS["USER_NAME"]="admin"
-UDF_VARS["DATABASE"]="mysql"
+UDF_VARS["SOA_EMAIL_ADDRESS"]="webmaster@${DEFAULT_DNS}"
 UDF_VARS["DISABLE_ROOT"]="No"
 UDF_VARS["SUBDOMAIN"]=""
 UDF_VARS["DOMAIN"]=""
