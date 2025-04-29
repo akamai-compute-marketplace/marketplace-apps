d6732bcabfb5dcd6da0bd8b778f6701018bfd61b
hmorris3293 <hmorris@akamai.com>
2025-04-29 09:16:00 -0400

    Update deployment_scripts/linode-marketplace-moodle/moodle-deploy.sh
    Co-authored-by: n0vabyte <94801247+n0vabyte@users.noreply.github.com>

--- deployment_scripts/linode-marketplace-moodle/moodle-deploy.sh
+++ deployment_scripts/linode-marketplace-moodle/moodle-deploy.sh
@@ -64,7 +64,7 @@ function provision_failed {
      -H "Content-Type: application/json" \
      -d "{ \"app_label\":\"${APP_LABEL}\", \"status\":\"provision_failed\", \"branch\": \"${BRANCH}\", \
         \"gituser\": \"${GH_USER}\", \"runjob\": \"${RUNJOB}\", \"image\":\"${IMAGE}\", \
-        \"type\":\"${TYPE}\", \"region\":\"${REGION}\" }"
+        \"type\":\"${TYPE}\", \"region\":\"${REGION}\", \"instance_env\":\"${INSTANCE_ENV}\" }"
   
   exit $?
 }
