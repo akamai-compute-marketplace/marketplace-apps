208c11fa6a17dd71918ba84e060217d97822803c
Joseph Cardillo <jocardil@akamai.com>
2025-09-10 14:23:25 -0400

    update test-vars

--- deployment_scripts/linode-marketplace-prometheus-grafana/test-vars.sh
+++ deployment_scripts/linode-marketplace-prometheus-grafana/test-vars.sh
@@ -18,14 +18,19 @@ UDF_VARS["USER_NAME"]="admin"
 UDF_VARS["DISABLE_ROOT"]="No"
 UDF_VARS["SUBDOMAIN"]=""
 UDF_VARS["DOMAIN"]=""
+UDF_VARS["TOKEN_PASSWORD"]=""
 UDF_VARS["SOA_EMAIL_ADDRESS"]="webmaster@${DEFAULT_DNS}"
+UDF_VARS["AKAMAI_CLIENT_SECRET"]="abcdEcSnaAt123FNkBxy456z25qx9Yp5CPUxlEfQeTDkfh4QA=I"
+UDF_VARS["AKAMAI_HOST"]="akab-lmn789n2k53w7qrs10cxy-nfkxaa4lfk3kd6ym.luna.akamaiapis.net"
+UDF_VARS["AKAMAI_ACCESS_TOKEN"]="akab-zyx987xa6osbli4k-e7jf5ikib5jknes3"
+UDF_VARS["AKAMAI_CLIENT_TOKEN"]="akab-nomoflavjuc4422-fa2xznerxrm3teg7"
 
 # dynamic variables
-#if [[ -n "${CHANGE_ME}" ]]; then
-#        UDF_VARS["CHANGE_ME"]="${CHANGE_ME}"
-#else
-#        UDF_VARS["CHANGE_ME"]="some value" # default
-#fi
+if [[ -n "${INSTALL_LOKI}" ]]; then
+        UDF_VARS["INSTALL_LOKI"]="${INSTALL_LOKI}"
+else
+        UDF_VARS["INSTALL_LOKI"]="No" # default
+fi
 
 set_vars() {
   for key in "${!UDF_VARS[@]}"; do
