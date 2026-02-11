c750d05e6a3492a1f10f28a3836e0f046009006d
ssastrau <ssastrau@akamai.com>
2026-02-11 12:47:58 +0100

    added configs

---
+++ deployment_scripts/linode-marketplace-cpanel-almalinux/linode-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+REGION="us-ord"
+LINODE_TYPE="g6-dedicated-4"
+IMAGE="linode/almalinux9"
+
+echo "REGION=${REGION}" >> "$GITHUB_ENV"
+echo "LINODE_TYPE=${LINODE_TYPE}" >> "$GITHUB_ENV"
+echo "IMAGE=${IMAGE}" >> "$GITHUB_ENV"
