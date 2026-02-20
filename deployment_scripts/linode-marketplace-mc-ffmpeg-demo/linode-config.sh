1345182cd55ef835bc3b077cbdfbae5c25f89da7
ssastrau <ssastrau@akamai.com>
2026-02-20 13:47:24 +0100

    added configs batch 6

---
+++ deployment_scripts/linode-marketplace-mc-ffmpeg-demo/linode-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+REGION="us-ord"
+LINODE_TYPE="g8-dedicated-16-8"
+IMAGE="linode/debian11"
+
+echo "REGION=${REGION}" >> "$GITHUB_ENV"
+echo "LINODE_TYPE=${LINODE_TYPE}" >> "$GITHUB_ENV"
+echo "IMAGE=${IMAGE}" >> "$GITHUB_ENV"
