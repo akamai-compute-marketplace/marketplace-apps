476f1388b9c9e1c4ea95259e62c172283ce0caf6
ssastrau <ssastrau@akamai.com>
2026-04-21 09:52:29 +0200

    upgrading apps

--- deployment_scripts/linode-marketplace-antmedia/linode-config.sh
+++ deployment_scripts/linode-marketplace-antmedia/linode-config.sh
@@ -4,7 +4,7 @@ set -e
 
 REGION="us-ord"
 LINODE_TYPE="g6-dedicated-4"
-IMAGE="linode/ubuntu22.04"
+IMAGE="linode/ubuntu24.04"
 
 echo "REGION=${REGION}" >> "$GITHUB_ENV"
 echo "LINODE_TYPE=${LINODE_TYPE}" >> "$GITHUB_ENV"
