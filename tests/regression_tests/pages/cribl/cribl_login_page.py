093b3c36dca323ebca0c4485d4bb6436b3b4dbb4
ssastrau <ssastrau@akamai.com>
2026-08-17 10:21:37 +0200

    fix cribl test locator

--- tests/regression_tests/pages/cribl/cribl_login_page.py
+++ tests/regression_tests/pages/cribl/cribl_login_page.py
@@ -8,8 +8,18 @@ class CriblLoginPage(BasePage):
         self.username_input = page.locator("#username")
         self.password_input = page.locator("#password")
         self.login_button = page.locator("button[type='submit']")
+        self.ai_workflows_continue_button = page.get_by_role(
+            "dialog", name="Introducing AI-accelerated workflows"
+        ).get_by_role("button", name="Continue")
 
     def login(self, username: str, password: str):
         self.username_input.fill(username)
         self.password_input.fill(password)
         self.login_button.click()
+        self.dismiss_ai_workflows_dialog()
+
+    def dismiss_ai_workflows_dialog(self):
+        try:
+            self.ai_workflows_continue_button.click(timeout=5000)
+        except Exception:
+            pass
